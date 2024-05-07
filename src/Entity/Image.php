511d794d1bb9d99be5d710c5fa176ce5822b902c
Nikiforova Mariya <manu11@bk.ru>
2024-05-07 23:35:07 +0700

    initial

---
+++ src/Entity/Image.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Image
+{
+    private string $url;
+
+    private int $size;
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+}
